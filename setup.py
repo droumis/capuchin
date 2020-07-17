81d5d0fc8b90d4d310af06ab150501e38047f95c
droumis <roumis.d@gmail.com>
2020-07-17 10:50:08 -0400

    env and setup stuff

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = ['numpy', 'numba', 'scipy', 'scikit-learn', 'matplotlib',
+                    'xarray', 'pandas', 'dask', 'tqdm', 'statsmodels', 'patsy',
+                    'holoviz']
+TESTS_REQUIRE = ['pytest >= 3.8.3']
+
+setup(
+    name='capuchin',
+    version='0.1.0.dev0',
+    license='MIT',
+    description=(''),
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+)
